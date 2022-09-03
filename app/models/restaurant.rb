2b6966f750f0a3e8e37d7cf5dba97f509bd824d2
itab ghazouani <itab.ghazouani@gmail.com>
2022-09-03 16:45:16 +0200

    Seed created and implemented

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,6 @@
 class Restaurant < ApplicationRecord
-  has_many :restaurant_dishes
+  has_many :restaurant_dishes, dependent: :destroy
+  has_many :dishes, through: :restaurant_dishes
   has_many :orders
-  has_many :bookings
+  has_many :bookings, dependent: :destroy
 end

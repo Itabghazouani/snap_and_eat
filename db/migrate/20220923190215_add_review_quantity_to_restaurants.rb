98b06a9f0bc1f728555a8f2c20674c8eb5e67898
itab ghazouani <itab.ghazouani@gmail.com>
2022-09-29 21:32:00 +0200

    remove and add review quantity column

--- db/migrate/20220923190215_add_review_quantity_to_restaurants.rb
+++ db/migrate/20220923190215_add_review_quantity_to_restaurants.rb
@@ -1,5 +1,6 @@
 class AddReviewQuantityToRestaurants < ActiveRecord::Migration[6.1]
   def change
+    remove_column :restaurants, :review_quantity
     add_column :restaurants, :review_quantity, :integer
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_24_150446) do
+ActiveRecord::Schema.define(version: 2022_09_28_202022) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 

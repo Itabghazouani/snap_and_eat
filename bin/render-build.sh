b3eed9d704ec5d58bcebeaec5cfbc57ee9ef191b
Itab Ghazouani <itab.ghazouani@gmail.com>
2024-09-19 15:14:21 +0200

    fixing error before deployment 3

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
-   # exit on error
-   set -o errexit
+# exit on error
+set -o errexit
 
-   bundle install
-   bundle exec rake assets:precompile
-   bundle exec rake assets:clean
-   bundle exec rake db:migrate
+bundle install
+yarn install
+bundle exec rake assets:precompile
+bundle exec rake assets:clean
+bundle exec rake db:migrate

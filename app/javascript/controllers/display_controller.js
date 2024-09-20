532cf534c6fae4a144301012bb099a7c5c9b77d3
Itab Ghazouani <itab.ghazouani@gmail.com>
2024-09-20 15:40:23 +0200

    fixing error before deployment 11

--- app/javascript/controllers/display_controller.js
+++ app/javascript/controllers/display_controller.js
@@ -3,8 +3,17 @@ import { Controller } from "stimulus"
 export default class extends Controller {
   static targets = [ "displayable" ]
 
+  connect() {
+    console.log("Display controller connected")
+  }
+
   display() {
-    console.log("hello");
-    this.displayableTarget.classList.remove("d-none");
+    console.log("Display method called")
+    if (this.hasDisplayableTarget) {
+      console.log("Toggling displayable")
+      this.displayableTarget.classList.toggle("d-none");
+    } else {
+      console.log("No displayable target found")
+    }
   }
 }
